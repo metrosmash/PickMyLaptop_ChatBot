b23f82db25308d58b2725734345019dfbbcf7856
Ajiboye Toluwalase <74120209+metrosmash@users.noreply.github.com>
2025-05-09 15:52:55 +0100

    solved markdown problem

--- streamlit_app.py
+++ streamlit_app.py
@@ -190,7 +190,7 @@ if prompt := st.chat_input("What can i do for you - "):
 response = chat.send_message(prompt)
 # Display assistant response in chat message container
 with st.chat_message("assistant"):
-    st.markdown(response)
+    st.markdown(response.text)
 # Add assistant response to chat history
 st.session_state.messages.append({"role": "assistant", "content": response})
 
