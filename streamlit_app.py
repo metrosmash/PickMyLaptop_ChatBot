a2e946547cf850bafdfbf4e5a61d1884116ce458
Ajiboye Toluwalase <74120209+metrosmash@users.noreply.github.com>
2025-05-16 17:01:40 +0100

    Code Organisation
    making the code beautiful

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+# Importing the libraries
 from google import genai
 from google.genai import types
 import streamlit as st
@@ -6,22 +7,11 @@ import pandas as pd
 import random
 import time
 import os
-
 import requests
 from io import StringIO
 
-# Show title and description.
-st.title("PickMyLaptop_Chatbot")
 
-st.write(
-    "This is a simple chatbot that uses Gemini flash 2.5 model to help users pick their preferred laptop. "
-    "To use this app, you need to provide a Gemini API key, which you can get [here]("
-    "https://ai.google.dev/gemini-api/docs/api-key)."
-    "You can also learn how to build this app step by step by [following our tutorial]("
-    "https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
-
-# .streamlit/secrets.toml
+# Config and Secrets
 # Retrieve credentials from Streamlit secrets
 
 db_username = st.secrets["DB_username"]
@@ -29,6 +19,7 @@ db_password = st.secrets["DB_password"]
 Gemini_Api_key = st.secrets["API_key"]
 
 
+# DB & Tools
 def query_sql_database(query: str):
     conn = None
     try:
@@ -76,7 +67,20 @@ with open("bot_prompt.txt", "r") as f:
 
 # result = query_sql_database("SELECT * FROM laptop_dataset LIMIT 5;")
 
-st.write("👋 Welcome to Laptop assistant Bot!")
+# Streamlit UI
+
+# Show title and description.
+st.title("PickMyLaptop_Chatbot V.0")
+# st.write(
+#     "This is a simple chatbot that uses Gemini flash 2.5 model to help users pick their preferred laptop. "
+#     "To use this app, you need to provide a Gemini API key, which you can get [here]("
+#     "https://ai.google.dev/gemini-api/docs/api-key)."
+#     "You can also learn how to build this app step by step by [following our tutorial]("
+#     "https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+# )
+st.write("👋 Welcome to Laptop assistant Bot!"
+         "This is a simple chatbot that uses Gemini flash 2.5 model to help users pick their preferred laptop. "
+         )
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -86,6 +90,7 @@ if "Gemini_model" not in st.session_state:
     st.session_state["Gemini_model"] = "gemini-2.0-flash"
 
 
+# Memory Setup
 def get_conversation_memory():
     """
     Returns previous user-agent message pairs as formatted string.
@@ -95,22 +100,22 @@ def get_conversation_memory():
     return context
 
 
+# Gemini Agent Setup
 assistant_function = [
     query_sql_database, get_conversation_memory
 ]
 
-model_name = "gemini-2.0-flash"
 
 client = genai.Client(api_key=Gemini_Api_key)
-
-
 chat = client.chats.create(
-    model=model_name,
+    model=st.session_state["Gemini_model"],
     config=types.GenerateContentConfig(
         tools=assistant_function,
         system_instruction=BOT_PROMPT
     ),
 )
+
+# Chat Logic
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
@@ -118,7 +123,6 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 # React to user input
-# Option 1
 if prompt := st.chat_input("What can i do for you - "):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
